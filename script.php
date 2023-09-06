949b1f7a0c48ec94f638ee979cee9a9769009525
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2023-09-06 20:29:29 +0300

    init

---
+++ script.php
@@ -0,0 +1,22 @@
+<?php ?>
+
+<! DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <title>first_try</title>
+        <style>
+            /* здесь будет код на css */
+            .head {
+                background-color: rgb(20, 20, 25);
+                color: pink;
+                font-family: Cursive;
+            }
+        </style>
+    </head>
+    <body class="head">
+        <header>
+            <h1>Нечаева Анна Анатольевна, гр R3238, вариант 1418</h1>
+        </header>
+    </body>
+</html>
